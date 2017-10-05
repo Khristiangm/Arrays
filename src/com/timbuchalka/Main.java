5bfa1b7d0beeccfc3cc7eeb9bd6e93e01c80dd0d
Khristian Guimaraes Marchioro (BFSI - Brazil) <KH389659@wipro.com>
2017-10-05 18:59:26 -0300

    first commit

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,71 @@
+package com.timbuchalka;
+
+import java.util.Scanner;
+
+public class Main {
+public static Scanner scanner = new Scanner(System.in);
+
+
+
+    public static void main(String[] args) {
+
+        int[] myIntegers = getIntegers(5);
+        for (int i = 0; i < myIntegers.length; i++){
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
+        }
+        System.out.println("The Average is " + getAverage(myIntegers));
+    }
+
+
+
+
+    public static int[] getIntegers(int number){
+        System.out.println("Enter " + number + " integer Values.\r");
+        int[] values = new int[number];
+        for (int i = 0; i < values.length; i++){
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
+
+    public static double getAverage(int[] array){
+        int sum = 0;
+        for (int i = 0; i < array.length; i++){
+
+            sum += array[i];
+        }
+        return (double) sum / (double) array.length;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        int[] myIntArray = new int[25];
+//        myIntArray[5] = 50;
+//        System.out.println(myIntArray[5]);
+//        for (int i = 0; i < myIntArray.length; i++) {
+//            myIntArray[i] = (i * 10);
+//            System.out.println("The Element " + i +" the Value Was " + myIntArray[i]);
+//        }
+//
+//        double[] myDoubleArray = new double[10];
+//        myDoubleArray[5] = 10;
+//        System.out.println(myDoubleArray[5]);
+//    }
+    }
+
